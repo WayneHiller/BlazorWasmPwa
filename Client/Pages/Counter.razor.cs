ebb92281f25567edf827ff18e76e8565dd6b5f58
Wayne Hiller <wayne.hiller@alliedinfo.net>
2020-10-16 09:13:53 -0700

    Prove Nullable Ref Type issue with component generic base class.

---
+++ Client/Pages/Counter.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPwa.Client.Pages
+{
+    public partial class Counter : CmpBase<string>
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
